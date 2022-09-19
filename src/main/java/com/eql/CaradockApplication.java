ba6cb7b2bb052579a0d2c7042bb06e6f0f4080f3
Briceae86 <104631909+Briceae86@users.noreply.github.com>
2022-09-19 14:30:39 +0200

    Début Caradock

---
+++ src/main/java/com/eql/CaradockApplication.java
@@ -0,0 +1,13 @@
+package com.eql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaradockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaradockApplication.class, args);
+	}
+
+}
